8de6362e02aeae163623477a2dc022e363345ad6
Michael Kennedy <merpso@gmail.com>
2021-02-19 21:23:42 +1100

    working code module, pulling from my vpc module

--- src/modules/awsInfra/vpc/outputs.tf
+++ src/modules/awsInfra/vpc/outputs.tf
@@ -1,5 +1,4 @@
 # VPC
-
 output "vpc_id" {
   description = "The ID of the VPC"
   value       = var.create_min ? module.vpc_min[0].vpc_id : module.vpc_max[0].vpc_id
@@ -15,63 +14,24 @@ output "private_subnets" {
   description = "List of IDs of private subnets"
   value = var.create_min ? module.vpc_min[0].private_subnets : module.vpc_max[0].private_subnets
 }
-/*
+
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = module.vpc_min[0].public_subnets
+  value       = var.create_min ? module.vpc_min[0].public_subnets : module.vpc_max[0].public_subnets
 }
 
 output "database_subnets" {
   description = "List of IDs of database subnets"
-  value       = module.vpc_min[0].database_subnets
+  value       = var.create_min ? module.vpc_min[0].database_subnets : module.vpc_max[0].database_subnets
 }
 
 output "intra_subnets" {
   description = "List of IDs of intra subnets"
-  value       = module.vpc_min[0].intra_subnets
+  value       = var.create_min ? module.vpc_min[0].intra_subnets : module.vpc_max[0].intra_subnets
 }
 
 # NAT gateways
 output "nat_public_ips" {
   description = "List of public Elastic IPs created for AWS NAT Gateway"
-  value       = module.vpc_min[0].nat_public_ips
+  value = var.create_min ? module.vpc_min[0].nat_public_ips : module.vpc_max[0].nat_public_ips
 }
-
-# VPC
-output "vpc_id_max" {
-  description = "The ID of the VPC"
-  value       = var.create_max ? module.vpc_max[0].vpc_id : null
-}
-/*
-output "vpc_cidr_block_max" {
-  description = "The CIDR block of the VPC"
-  value       = concat(module.vpc_max[0].*.vpc_cidr_block,[""])[0]
-}
-
-# Subnets
-output "private_subnets_max" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc_max[0].private_subnets
-}
-
-output "public_subnets_max" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc_max[0].public_subnets
-}
-
-output "database_subnets_max" {
-  description = "List of IDs of database subnets"
-  value       = module.vpc_max[0].database_subnets
-}
-
-output "intra_subnets_max" {
-  description = "List of IDs of intra subnets"
-  value       = module.vpc_max[0].intra_subnets
-}
-
-# NAT gateways
-output "nat_public_ips_max" {
-  description = "List of public Elastic IPs created for AWS NAT Gateway"
-  value       = module.vpc_max[0].nat_public_ips
-}
-*/
